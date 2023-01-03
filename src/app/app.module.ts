c70393e11c6b8ccef4d8c3c90212e4a46f4eb4c3
francescafilice <francescafilice.ff@libero.it>
2023-01-03 15:59:23 +0100

    Aggiunto compnente registrationForm e dipendenza ReactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import appRoutes from "./appRoutes";
 import {RouterModule} from "@angular/router";
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
