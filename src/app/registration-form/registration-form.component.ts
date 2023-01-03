07fb3611cb23460641b8e066c5e120524849d744
francescafilice <francescafilice.ff@libero.it>
2023-01-03 16:28:18 +0100

    Inizializzato interfaccia form

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
-export class RegistrationFormComponent {
+
+export class RegistrationFormComponent implements OnInit {
+  registrationForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+  }
+
+  get email() { return this.registrationForm.get('email'); }
+  get username() { return this.registrationForm.get('username'); }
+  get password() { return this.registrationForm.get('password'); }
 
 }
