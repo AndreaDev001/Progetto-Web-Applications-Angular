5b343270e58f31a9f3fc4708ae4d668466d59e1c
PierKnight <pier.altimari@libero.it>
2023-01-04 15:37:39 +0100

    added utility files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,70 @@
 import { NgModule } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ReviewComponent } from './review/review.component';
+import { CommentComponent } from './comment/comment.component';
+
 import appRoutes from "./appRoutes";
 import {RouterModule} from "@angular/router";
+import { NgxEditorModule } from 'ngx-editor';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+import {AutosizeModule} from 'ngx-autosize';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    InfiniteScrollModule,
+    NgxEditorModule.forRoot({
+      locals: {
+        // menu
+        bold: 'Bold',
+        italic: 'Italic',
+        code: 'Code',
+        blockquote: 'Blockquote',
+        underline: 'Underline',
+        strike: 'Strike',
+        bullet_list: 'Bullet List',
+        ordered_list: 'Ordered List',
+        heading: 'Heading',
+        h1: 'Header 1',
+        h2: 'Header 2',
+        h3: 'Header 3',
+        h4: 'Header 4',
+        h5: 'Header 5',
+        h6: 'Header 6',
+        align_left: 'Left Align',
+        align_center: 'Center Align',
+        align_right: 'Right Align',
+        align_justify: 'Justify',
+        text_color: 'Text Color',
+        background_color: 'Background Color',
+
+        // popups, forms, others...
+        url: 'URL',
+        text: 'Text',
+        openInNewTab: 'Open in new tab',
+        insert: 'Insert',
+        altText: 'Alt Text',
+        title: 'Title',
+        remove: 'Remove',
+      },
+    }),
+    FormsModule,
+    AutosizeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/appRoutes.ts
+++ src/app/appRoutes.ts
@@ -1,4 +1,8 @@
-import {Routes} from "@angular/router";
+import { Routes } from "@angular/router";
+import {ReviewComponent} from "./review/review.component";
+import { reviewResolver } from "./review/review.service";
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  {title: "CREAZIONE RECENSIONE", path: ":gameID/newRecensione", component: ReviewComponent},
+  {title: "RECENSIONE",resolve: {review: reviewResolver}, path: "recensioni/:reviewID", component: ReviewComponent}];
 export default appRoutes;
