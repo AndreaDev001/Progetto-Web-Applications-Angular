c018198b733a79277eae7eb366439954424ae3ca
francescafilice <francescafilice.ff@libero.it>
2023-01-04 14:54:48 +0100

    Aggiunta protezione pagine

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -5,11 +5,20 @@ import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthenticationService {
 
   private url : string = "http://localhost:8080";
 
   constructor(private http: HttpClient) { }
+
+
+  // da inserire in tutte quelle pagine angular che possono essere visualizzate solo da chi è loggato
+  checkLogin(jsessionid: string): Observable<Boolean> {
+    // chiamo la servlet mappata con /checkAuth e le passo la jsessionid
+    // restituisce true se l'utente è loggato, false altrimenti
+    return this.http.get<Boolean>(this.url + "/checkAuth", {params: {jsessionid: jsessionid}});
+  }
 
   // contatta il REST controller Registrationhandler con una post mappata su /doRegistration,
   // nel cui corpo inserisce email, username e password
