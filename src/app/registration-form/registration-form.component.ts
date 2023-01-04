fe57e075c8825455a382d861aa11e38884bdc035
francescafilice <francescafilice.ff@libero.it>
2023-01-04 09:47:45 +0100

    Aggiunto alert requisiti password

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -29,7 +29,12 @@ export class RegistrationFormComponent implements OnInit {
   }
 
   showPasswordRequirements(): void {
-    // todo
+    alert('Password must contain at least:\n'+
+      '- a lowercase character,\n' +
+      '- an uppercase character,\n' +
+      '- a special character,\n' +
+      '- a digit\n' +
+      'Password can\'t contain space and must be at least 8 characters long');
   }
 
   goToLogin(): void {
