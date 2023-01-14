aff87a7e80f2e9df50ff3d85a6e1c9e16e728550
francescafilice <francescafilice.ff@libero.it>
2023-01-14 17:20:26 +0100

    Aggiunto routing e root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import appRoutes from "./appRoutes";
 import {RouterModule} from "@angular/router";
+import { NewsComponent } from './news/news.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
