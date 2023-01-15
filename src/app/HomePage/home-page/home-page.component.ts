f01cfed8cfffcd94da8a1b60ffe12a742d9eba27
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2023-01-15 21:01:26 +0100

    Bug Fix

--- src/app/HomePage/home-page/home-page.component.ts
+++ src/app/HomePage/home-page/home-page.component.ts
@@ -1,19 +1,28 @@
-import {Component,OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GameHandlerService} from "../../services/game-handler.service";
 import {NgxSpinnerService} from "ngx-spinner";
-import {skip} from "rxjs";
+import {skip, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit{
+export class HomePageComponent implements OnInit,OnDestroy{
+
+  private subscriptions: Subscription[] = [];
   constructor(private gameHandler: GameHandlerService,private spinnerService: NgxSpinnerService) {
 
   }
-  public ngOnInit(): void{
-    this.spinnerService.show();
-    this.gameHandler.getGenres(false).pipe(skip(1)).subscribe((value: any) => this.spinnerService.hide());
+  public ngOnInit(): void
+  {
+      this.spinnerService.show();
+      if(!this.gameHandler.getGenres(true))
+          this.subscriptions.push(this.gameHandler.getGenres(false).pipe(skip(1)).subscribe((value: any) => this.spinnerService.hide()));
+      else
+         this.spinnerService.hide();
+  }
+  public ngOnDestroy(): void{
+    this.subscriptions.forEach((value: Subscription) => value.unsubscribe());
   }
 }
