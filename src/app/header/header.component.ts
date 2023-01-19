72b5036a586b4bd0217b3b2af1ae72118f5f39dd
Andrea <69543567+AndreaDev001@users.noreply.github.com>
2023-01-19 11:51:59 +0100

    Modificato header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {SpringHandlerService} from "../services/spring-handler.service";
+import {Utente} from "../interfaces";
+import {faCircleInfo, faCircleUser, faHouse, faNewspaper, IconDefinition} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit,OnDestroy{
 
+  @Input() isLogged: boolean = false;
+  private subscriptions: Subscription[] = [];
+  public icons: IconDefinition[] = [faHouse,faNewspaper,faCircleInfo,faCircleUser];
+  constructor(private springHandler: SpringHandlerService) {
+
+  }
+  public ngOnInit(): void{
+    this.subscriptions.push(this.springHandler.getCurrentUsername(false).subscribe((value: Utente) => this.isLogged = value != undefined));
+  }
+  public ngOnDestroy(): void{
+    this.subscriptions.forEach((value: Subscription) => value.unsubscribe());
+  }
+  public createLink(requiredPage: string): string{
+    return "http://localhost:8080/" + requiredPage + "?" + "jsessionid=" + this.springHandler.getSessionID(true);
+  }
 }
